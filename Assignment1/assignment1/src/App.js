2faf58ea4d03f3715cee8eb69a4c6c7d9835c7bf
Mudra Verma <md393822@dal.ca>
2023-05-28 02:47:06 -0300

    Header and Footer

---
+++ Assignment1/assignment1/src/App.js
@@ -0,0 +1,30 @@
+import Header from './Header';
+import Footer from './Footer';
+import './styles.css';
+// import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Header/>
+      <Footer/>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ Assignment1/assignment1/src/Header.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './styles.css';
+
+function Header(){
+    return(
+        <div class = 'Frame'>
+            <div class = 'Header'>
+                <div class = 'BlankCol'/>
+                <div class = 'LogoCol'>
+                    <div class = 'Circle'>
+                        <img src={logo} class="AppLogo" alt="logo"/>
+                    </div>
+                </div>
+                <div class = 'LoginCol'>
+                    <div class = 'ButtonP'>
+                        Login/SignUp
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Header;
