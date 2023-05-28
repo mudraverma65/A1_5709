c09291c9146ebb927e6d1e481dae2677f6394c33
Mudra Verma <md393822@dal.ca>
2023-05-28 19:53:38 -0300

    Category Cards Aligned and media inserted

--- Assignment1/assignment1/src/Categories.js
+++ Assignment1/assignment1/src/Categories.js
@@ -1,5 +1,11 @@
 import './styles.css';
 import electricians from './electrician.png';
+import plumber from './plumber.png';
+import writer from './writer.png';
+import finance from './finance.png';
+import architect from './architect.png';
+import it from './it.png';
+
 
 function Categories(){
     return(
@@ -8,23 +14,46 @@ function Categories(){
             <div class = 'CategoryRow'>
                 <div class = 'CategoryCard'>
                     <div class = 'CategoryIcon'>
-                        <img src={electricians} class="electricians" alt="logo"/>
+                        <img src={electricians} class="category-logo" alt="logo"/>
                     </div>
                     <div class = 'CategoryTitle'><h3>Electricians</h3></div>
                     <div class = 'ButtonS'>View all</div>
                 </div>
                 <div class = 'CategoryCard'>
                     <div class = 'CategoryIcon'>
-                        <img src={electricians} class="electricians" alt="logo"/>
+                        <img src={plumber} class="category-logo" alt="logo"/>
                     </div>
-                    <div class = 'CategoryTitle'><h3>Electricians</h3></div>
+                    <div class = 'CategoryTitle'><h3>Plumber</h3></div>
                     <div class = 'ButtonS'>View all</div>
                 </div>
                 <div class = 'CategoryCard'>
                     <div class = 'CategoryIcon'>
-                        <img src={electricians} class="electricians" alt="logo"/>
+                        <img src={it} class="category-logo" alt="logo"/>
                     </div>
-                    <div class = 'CategoryTitle'><h3>Electricians</h3></div>
+                    <div class = 'CategoryTitle'><h3>IT & Software</h3></div>
+                    <div class = 'ButtonS'>View all</div>
+                </div>
+            </div>
+            <div class = 'CategoryRow'>
+                <div class = 'CategoryCard'>
+                    <div class = 'CategoryIcon'>
+                        <img src={writer} class="category-logo" alt="logo"/>
+                    </div>
+                    <div class = 'CategoryTitle'><h3>Writer</h3></div>
+                    <div class = 'ButtonS'>View all</div>
+                </div>
+                <div class = 'CategoryCard'>
+                    <div class = 'CategoryIcon'>
+                        <img src={architect} class="category-logo" alt="logo"/>
+                    </div>
+                    <div class = 'CategoryTitle'><h3>Architect</h3></div>
+                    <div class = 'ButtonS'>View all</div>
+                </div>
+                <div class = 'CategoryCard'>
+                    <div class = 'CategoryIcon'>
+                        <img src={finance} class="category-logo" alt="logo"/>
+                    </div>
+                    <div class = 'CategoryTitle'><h3>Finance</h3></div>
                     <div class = 'ButtonS'>View all</div>
                 </div>
             </div>
