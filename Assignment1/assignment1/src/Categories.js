61542c04bbf8f8ddd83213466539d5328bcab8a9
Mudra Verma <md393822@dal.ca>
2023-05-29 12:43:58 -0300

    Landing page mobile responsive

--- Assignment1/assignment1/src/Categories.js
+++ Assignment1/assignment1/src/Categories.js
@@ -17,21 +17,27 @@ function Categories(){
                         <img src={electricians} class="category-logo" alt="logo"/>
                     </div>
                     <div class = 'CategoryTitle'><h3>Electricians</h3></div>
-                    <div class = 'ButtonS'>View all</div>
+                    <div class = 'CategoryButton'>
+                        <div class = 'ButtonS'>View all</div>
+                    </div>
                 </div>
                 <div class = 'CategoryCard'>
                     <div class = 'CategoryIcon'>
                         <img src={plumber} class="category-logo" alt="logo"/>
                     </div>
                     <div class = 'CategoryTitle'><h3>Plumber</h3></div>
-                    <div class = 'ButtonS'>View all</div>
+                    <div class = 'CategoryButton'>
+                        <div class = 'ButtonS'>View all</div>
+                    </div>
                 </div>
                 <div class = 'CategoryCard'>
                     <div class = 'CategoryIcon'>
                         <img src={it} class="category-logo" alt="logo"/>
                     </div>
                     <div class = 'CategoryTitle'><h3>IT & Software</h3></div>
-                    <div class = 'ButtonS'>View all</div>
+                    <div class = 'CategoryButton'>
+                        <div class = 'ButtonS'>View all</div>
+                    </div>
                 </div>
             </div>
             <div class = 'CategoryRow'>
@@ -40,23 +46,34 @@ function Categories(){
                         <img src={writer} class="category-logo" alt="logo"/>
                     </div>
                     <div class = 'CategoryTitle'><h3>Writer</h3></div>
-                    <div class = 'ButtonS'>View all</div>
+                    <div class = 'CategoryButton'>
+                        <div class = 'ButtonS'>View all</div>
+                    </div>
                 </div>
                 <div class = 'CategoryCard'>
                     <div class = 'CategoryIcon'>
                         <img src={architect} class="category-logo" alt="logo"/>
                     </div>
                     <div class = 'CategoryTitle'><h3>Architect</h3></div>
-                    <div class = 'ButtonS'>View all</div>
+                    <div class = 'CategoryButton'>
+                        <div class = 'ButtonS'>View all</div>
+                    </div>
                 </div>
                 <div class = 'CategoryCard'>
                     <div class = 'CategoryIcon'>
                         <img src={finance} class="category-logo" alt="logo"/>
                     </div>
                     <div class = 'CategoryTitle'><h3>Finance</h3></div>
-                    <div class = 'ButtonS'>View all</div>
+                    <div class = 'CategoryButton'>
+                        <div class = 'ButtonS'>View all</div>
+                    </div>
+                    
                 </div>
             </div>
+            <div class = 'DisplayAll'>
+                <div class = 'ButtonP'>View All Listings</div>
+            </div>
+            
         </div>
 
     );
